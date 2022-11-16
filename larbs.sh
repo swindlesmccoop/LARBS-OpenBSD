be8e39ca84a5c75275aa9fbcd7557dd5fa686a97
testing <testing@hostname.my.domain>
2022-11-16 14:29:05 -0500

    Port update + a million other things

--- larbs.sh
+++ larbs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -x
 # Luke's Auto Rice Boostrapping Script (LARBS)
 # by Luke Smith <luke@lukesmith.xyz>
 # and Swindles McCoop <swindlesmccoop@waifu.club>
@@ -7,8 +7,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/swindlesmccoop/voidrice-openbsd.git"
-progsfile="https://raw.githubusercontent.com/swindlesmccoop/LARBS-OpenBSD/master/progs.csv"
+dotfilesrepo="https://git.cbps.xyz/swindlesmccoop/voidrice-openbsd.git"
+progsfile="https://git.cbps.xyz/swindlesmccoop/LARBS-OpenBSD/raw/branch/master/progs.csv"
 repobranch="master"
 
 ### FUNCTIONS ###
@@ -30,7 +30,6 @@ welcomemsg() {
 	dialog --title "Important Note!" --yes-button "All ready!" \
 		--no-button "Return..." \
 		--yesno "IGNORE MSG" 8 70
-		#--yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
 }
 
 getuserandpass() {
@@ -76,49 +75,15 @@ adduserandpass() {
 	unset pass1 pass2
 }
 
-portinstall() {
+suckinstall(){
 	cd /usr/ports/mystuff/$1
-	doas make
-	doas make install
+	make && make install
 }
 
 maininstall() {
 	# Installs all needed programs from main repo.
 	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
 	installpkg "$1"
-}
-
-#old_gitmakeinstall() {
-#	progname="${1##*/}"
-#	progname="${progname%.git}"
-#	dir="$repodir/$progname"
-#	dialog --title "LARBS Installation" \
-#		--infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
-#	doas -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
-#		--no-tags -q "$1" "$dir" ||
-#		{
-#			cd "$dir" || return 1
-#			doas -u "$name" git pull --force origin master
-#		}
-#	cd "$dir" || exit 1
-#	gmake >/dev/null 2>&1
-#	gmake install >/dev/null 2>&1
-#	cd /tmp || return 1
-#}
-
-gitmakeinstall() {
-	dialog --title "LARBS Installation" \
-	--infobox "Installing suckless builds ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
-	cd $repodir
-	git clone https://github.com/swindlesmccoop/luke-dwm-openbsd dwm
-	git clone https://github.com/swindlesmccoop/luke-dwmblocks-openbsd dwmblocks
-	git clone https://github.com/swindlesmccoop/luke-st-openbsd st
-	git clone https://github.com/swindlesmccoop/luke-dmenu-openbsd dmenu
-
-	cd dwm && gmake && gmake install && cd ..
-	cd dwmblocks && gmake && gmake install && cd ..
-	cd st && gmake && gmake install && cd ..
-	cd dmenu && gmake && gmake install && cd ..
 }
 
 pipinstall() {
@@ -129,34 +94,20 @@ pipinstall() {
 }
 
 installationloop() {
+	set -x
 	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) ||
 		curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
 	total=$(wc -l </tmp/progs.csv)
-	aurinstalled=$(pacman -Qqm)
 	while IFS=, read -r tag program comment; do
 		n=$((n + 1))
 		echo "$comment" | grep -q "^\".*\"$" &&
 			comment="$(echo "$comment" | sed -E "s/(^\"|\"$)//g")"
 		case "$tag" in
-		#"G") gitmakeinstall "$program" "$comment" ;;
 		"P") pipinstall "$program" "$comment" ;;
-		"R") portinstall "$program" "$comment" ;;
+		"S") suckinstall "$program" ;;
 		*) maininstall "$program" "$comment" ;;
 		esac
 	done </tmp/progs.csv
-}
-
-putgitrepo() {
-	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	dialog --infobox "Downloading and installing config files..." 7 60
-	[ -z "$3" ] && branch="master" || branch="$repobranch"
-	dir=$(mktemp -d)
-	[ ! -d "$2" ] && mkdir -p "$2"
-	chown "$name":wheel "$dir" "$2"
-	doas -u "$name" git -C "$repodir" clone --depth 1 \
-		--single-branch --no-tags -q --recursive -b "$branch" \
-		--recurse-submodules "$1" "$dir"
-	doas -u "$name" cp -rfT "$dir" "$2"
 }
 
 vimplugininstall() {
@@ -211,27 +162,33 @@ adduserandpass || error "Error adding username and/or password."
 
 # Allow user to run doas without password. Since ports must be installed
 # in a fakeroot environment, this is required for all builds with the ports tree.
-printf "permit nopass root\npermit nopass $USER\n" > /etc/doas.conf
+printf "permit nopass root\npermit nopass $name\n" > /etc/doas.conf
 
 # The command that does all the installing. Reads the progs.csv file and
 # installs each needed program the way required. Be sure to run this only after
 # the user has been created and has priviledges to run sudo without a password
 # and all build dependencies are installed.
+
+[ ! -d /usr/ports/ ] && git clone --depth 1 https://github.com/openbsd/ports /usr/ports/
+[ ! -d /usr/ports/mystuff ] && git clone --depth 1 https://github.com/swindlesmccoop/openbsd-ports /usr/ports/mystuff/
+
 installationloop
-gitmakeinstall
 
 # Install the dotfiles in the user's home directory, but remove .git dir and
 # other unnecessary files.
-putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
+dialog --infobox "Downloading and installing config files..." 7 60
+[ -z "$3" ] && branch="master" || branch="$repobranch"
+[ ! -d "$2" ] && mkdir -p "$2"
+chown "$name":wheel "$dir" "$2"
+doas -u "$name" git -C "$repodir" clone --depth 1 \
+	--single-branch --no-tags -q --recursive -b "$branch" \
+	--recurse-submodules "$1" "$dir"
+doas -u "$name" cp -rfT "$dir" "$2"
+
 rm -rf "/home/$name/.git/" "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
 
 # Install vim plugins if not alread present.
 [ ! -f "/home/$name/.config/nvim/autoload/plug.vim" ] && vimplugininstall
-
-# Most important command! Get rid of the beep!
-# Sorry Luke, not sure of a way to do this on OpenBSD. --Swindles
-#rmmod pcspkr
-#echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
 
 # Make zsh the default shell for the user.
 chsh -s /usr/local/bin/zsh "$name"
@@ -242,9 +199,6 @@ doas -u "$name" mkdir -p "/home/$name/.config/mpd/playlists/"
 # dbus UUID must be generated for Artix runit.
 dbus-uuidgen >/var/lib/dbus/machine-id
 
-# Use system notifications for Brave on Artix
-echo "export \$(dbus-launch)" >/etc/profile.d/dbus.sh
-
 # Enable tap to click
 [ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
         Identifier "libinput touchpad catchall"
@@ -254,12 +208,6 @@ echo "export \$(dbus-launch)" >/etc/profile.d/dbus.sh
 	# Enable left mouse button by tapping
 	Option "Tapping" "on"
 EndSection' >/usr/X11R6/share/X11/xorg.conf.d/40-libinput.conf
-
-# Allow wheel users to sudo with password and allow several system commands
-# (like `shutdown` to run without password).
-#echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/00-larbs-wheel-can-sudo
-#echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -u -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-larbs-cmds-without-password
-#echo "Defaults editor=/usr/bin/nvim" >/etc/sudoers.d/02-larbs-visudo-editor
 
 # Last message! Install complete!
 finalize
